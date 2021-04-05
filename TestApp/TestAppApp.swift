13615d1f0e1706f773d65ae405acab66b9ef488d
Alberto Aldana <albertoaldanar@hotmail.com>
2021-04-05 15:51:13 -0600

    initialized test app

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Alberto Aldana on 05/04/21.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
